04aba6e492e116cdba020fabf480a35d7abd18cf
ian287913 <45512766+ian287913@users.noreply.github.com>
2020-06-15 01:49:33 +0800

    add lancer

--- Source/ArrayTexture/AnimationConfig.h
+++ Source/ArrayTexture/AnimationConfig.h
@@ -46,8 +46,10 @@ Super	(high damage & health, kb-resist)
 
 vector<AnimationConfig> AnimationConfigTable{
 	//{ "L_Basic", "/Lionar/f1_tank.png" , 12, 6},
+	{ "L_Minion"	, "/Lionar/f1_silverguardsquire.png", 12, 4		, 0.0f, 0.25f, 2.0f		, 0.02f, -0.33f, 0.56f},
 	{ "L_Tank"	, "/Lionar/f1_tank.png"					, 12, 6		, 0.0f, 0.25f, 1.0f		, 0.02f, -0.3f, 0.56f},
 	{ "L_Ranger", "/Lionar/f1_backlinearcher.png"		, 8, 16		, 0.0f, 0.35f, 1.0f		, 0.04f, -0.33f, 0.45f},
+	{ "L_Lancer"	, "/Lionar/f1_radiantdragoon.png"	, 10, 8		, 0.0f, 0.32f, 3.4f		, -0.02f, -0.34f, 0.26f},
 	{ "L_Tower"	, "/Lionar/f1_ironcliffemonument.png"	, 10, 10	, 0.0f, 0.3f, 1.0f		, 0.0f, -0.33f, 0.56f},
 	{ "A_Tower"	, "/Abyssian/f4_buildlegendary.png"		, 10, 10	, 0.0f, 0.3f, 5.0f		, 0.0f, -0.32f, 0.4f},
 	{ "A_Minion"	, "/Abyssian/neutral_shadow1.png"	, 12, 4		, 0.0f, 0.27f, 2.8f		, 0.0f, -0.28f, 0.3f},
@@ -56,7 +58,6 @@ vector<AnimationConfig> AnimationConfigTable{
 	
 	
 	
-
 	{ "Icon_Ultimate"		, "/Icon/icon_f1_tempest.png"	, 5, 5		, 0.0f, 0.0f, 1.0f		, 0.0f, 0.0f, 0.0f},
 	{ "FX_BeamFire"			, "/FX/fx_beamfire.png"				, 5, 7		, 0.0f, 0.4f, 9.0f		, 0.0f, 0.0f, 0.0f},
 	{ "FX_BeamLaser"		, "/FX/fx_beamlaser.png"			, 2, 17		, 0.0f, 0.4f, 7.0f		, 0.0f, 0.0f, 0.0f},
@@ -64,6 +65,13 @@ vector<AnimationConfig> AnimationConfigTable{
 };
 
 vector<AnimFrameSet> AnimFrameSetTable{
+	{ "L_Minion", "idle",		{	{1, 11}, {1, 4},  }, {}, 2, true },
+	{ "L_Minion", "run",		{	{0, 7}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}}, {}, 6, true },
+	{ "L_Minion", "attack",		{	{1, 11}, {3, 9}, {3, 8}, {3, 7}, {3, 6}, {3, 5}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {2, 11}, {2, 10},  }, {}, 12, false, "idle" },
+	{ "L_Minion", "hit",		{	{1, 11}, {1, 6}, {1, 5},  }, {}, 3, false, "idle" },
+	{ "L_Minion", "die",		{	{1, 11}, {2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {3, 10}, {1, 10}, {1, 9}, {1, 8}, {1, 7},  }, {}, 6, false },
+
+
 	{ "L_Tank", "idle",		{	{2, 3}, {1, 5}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {0, 11}, {0, 10}, {0, 9}, {0, 8},  }, {}, 6, true },
 	{ "L_Tank", "run",		{	{0, 7}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}}, {}, 6, true },
 	{ "L_Tank", "hit",		{	{1, 8}, {1, 7}, {1, 8},  }, {}, 1, false, "idle" },
@@ -78,6 +86,14 @@ vector<AnimFrameSet> AnimFrameSetTable{
 	{ "L_Ranger", "hit",		{	{3, 5}, {3, 4}, {3, 2}}, {}, 6, true },
 	{ "L_Ranger", "attack",		{	{7, 2}, {7, 1}, {7, 0}, {6, 7}, {6, 6}, {6, 5}, {6, 4}, {6, 3}, {6, 2}, {6, 1}, {6, 0}, {5, 7}, {5, 6}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {5, 1}, {5, 0}, {4, 7}, {4, 6}, {4, 5}, {4, 4}, {4, 3}}, {}, 10, false, "idle" },
 	{ "L_Ranger", "die",		{	{3, 5}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {2, 7}, {2, 6}, {2, 5}, {2, 4}, {2, 3}, {2, 2}, {2, 1}, {2, 0}, {1, 7}}, {}, 6, false },
+
+	//...
+	{ "L_Lancer", "idle",		{	{1, 9}, {1, 8}, {1, 7}, {1, 6}, {1, 5}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {0, 9}, {0, 8},  }, {}, 6, true },
+	{ "L_Lancer", "run",		{	{0, 7}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}}, {}, 6, true },
+	{ "L_Lancer", "attack",		{	{3, 5}, {7, 0}, {6, 9}, {6, 8}, {6, 7}, {6, 6}, {6, 5}, {6, 4}, {6, 3}, {6, 2}, {6, 1}, {6, 0}, {5, 9}, {5, 8}, {5, 7}, {5, 6}, {5, 5}, {5, 4}, {5, 3}, {5, 2}, {5, 1}, {5, 0}, {4, 9}, {4, 8}, {4, 7},  }, {}, 12, false, "idle" },
+	{ "L_Lancer", "hit",		{	{3, 6}, {7, 1}, {2, 0},  }, {}, 3, false, "idle" },
+	{ "L_Lancer", "die",		{	{3, 6}, {7, 1}, {3, 4}, {3, 3}, {3, 2}, {3, 1}, {3, 0}, {2, 9}, {2, 8}, {2, 7}, {2, 6}, {2, 5}, {2, 4}, {2, 3}, {2, 2}, {2, 1},  }, {}, 6, false },
+
 
 
 	{ "L_Tower", "idle",		{	{3, 3}, {1, 4}, {1, 3}, {1, 2}, {1, 1}, {1, 0}, {0, 9}, {0, 8}, {0, 7}, {0, 6}, {0, 5}, {0, 4}, {0, 3}, {0, 2}, {0, 1}, {0, 0}}, {}, 6, true },
